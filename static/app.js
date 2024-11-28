0bc91dbc1b22127bad052f4b72e97822b9763c3c
Kenbachkhoa1234 <Kn25122004@gmail.com>
2024-11-28 13:33:49 +0700

    Cập nhật app.js với sửa lỗi Socket.IO1

--- static/app.js
+++ static/app.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://127.0.0.1:5000');
+var socket = io.connect('https://btaplon-zwj7.vercel.app/');
 var socket = io();
 socket.on('update', function(data) {
     var resultDiv = document.getElementById('result');
