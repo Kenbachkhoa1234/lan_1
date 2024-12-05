2fd2f446a569b421cf3e7cc3d70a7cabcdabcbfc
Kenbachkhoa1234 <Kn25122004@gmail.com>
2024-12-05 14:01:34 +0700

    su1su1

--- static/app.js
+++ static/app.js
@@ -1,4 +1,4 @@
-var socket = io.connect('https://lan-1-umsc.vercel.app/');
+var socket = io.connect('http://127.0.0.1:5000/');
 socket.on('update', function(data) {
     var resultDiv = document.getElementById('result');
     if (data.type === 'produced') {
